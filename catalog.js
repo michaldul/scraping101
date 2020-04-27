7dba12aa9c728c2814aaec254fdf3f4cf7615fe7
Michal Dul <mdul@shoprunner.com>
2020-04-27 15:30:23 +0200

    final state

--- catalog.js
+++ catalog.js
@@ -5,12 +5,8 @@ const { urls } = require("./urls");
 const { scrapeProduct } = require("./scrape");
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    from(urls).pipe(
-        mergeMap(url => scrapeProduct(browser, url), undefined, 2)
-    ).subscribe(
-        product => console.log(product),
-        err => console.error(`error ${err}`),
-        () => { browser.close(); }
+    const browser = await puppeteer.launch({headless: false});
+    from(urls).pipe(mergeMap(url => scrapeProduct(browser, url), undefined, 1)).subscribe(
+        product => console.log(product), err => console.err(err), () => browser.close()
     )
 })();
--- scrape.js
+++ scrape.js
@@ -1,31 +1,31 @@
-const puppeteer = require("puppeteer")
-const sleep = require("util").promisify(setTimeout);
+const puppeteer = require("puppeteer");
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-    const url = 'https://kubotastore.pl/p/KK1806/klapki-kubota-premium-tricolor/KK1806-45';
-    
-    const product = await scrapeProduct(browser, url);
-
-    console.log(product)
-    await sleep(500);
-    await browser.close();
-})();
+// (async () => {
+//     const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+//     const url = 'https://kubotastore.pl/p/KK1806/klapki-kubota-premium-tricolor/KK1806-45';
+//     const product = await scrapeProduct(browser, url);
+//     console.log(product);
+//     await browser.close();
+// })();
 
 async function scrapeProduct(browser, url) {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1200, height: 800 })
-    await page.goto(url);
     var product = {url: url}
+    const page = await browser.newPage();
+    await page.setViewport({width: 1200, height: 800});
+    await page.goto(url);
+    // get title
+    const element = await page.$('.product__details__title')
+    product.title = (await page.evaluate(el => el.textContent, element)).trim();
+
+    // get price
+    product.price = await page.$eval('.product__details__price', el => el.textContent)
     
-    const titleElement = await page.$(".product__details__title");
-    product.title = (await page.evaluate(element => element.textContent, titleElement)).trim();
-    product.price = await page.$eval(".product__details__price", 
-        element => element.textContent);
-    product.sizes = await page.$$eval(".size-select__other-sizes--size-option",
-        elements => elements.map(element => element.textContent));
+    // get sizes
+    product.sizes = await page.$$eval('.size-select__other-sizes--size-option', elements => elements.map(el => el.textContent));
     
-    page.close()
+    // close page
+    await page.close();
+
     return product;
 }
 
